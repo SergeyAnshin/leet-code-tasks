5feebd7563bcb7357437d2270d2e7298c7d3ebbc
SergeyAnshin <anshin.serega@gmail.com>
2022-10-17 11:56:26 +0300

    add PalindromeNumber task

---
+++ src/easy/task1/PalindromeNumber.java
@@ -0,0 +1,21 @@
+package easy.task1;
+
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        return String.valueOf(x)
+                .contentEquals(new StringBuilder().append(x).reverse());
+    }
+
+    public boolean isPalindromeBestSolution(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+}
